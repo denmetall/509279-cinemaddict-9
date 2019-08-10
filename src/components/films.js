b25e62f3843d5f986a74a3cce878c08b4a6478c3
Denmetall <denmetall@yandex.ru>
2019-08-10 22:26:51 +0700

    done module2-task1

--- src/components/films.js
+++ src/components/films.js
@@ -1,6 +1,3 @@
-import {getCardTemplate} from "./film-card";
-import {getBtnMoreTemplate} from "./btn-more";
-
 export const getFilmsTemplate = () => {
   return `
     <section class="films">
@@ -8,22 +5,16 @@ export const getFilmsTemplate = () => {
         <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
   
         <div class="films-list__container">
-          ${getCardTemplate()}
-          ${getCardTemplate()}
-          ${getCardTemplate()}
-          ${getCardTemplate()}
-          ${getCardTemplate()}
+
         </div>
-  
-        ${getBtnMoreTemplate()}
+
       </section>
   
       <section class="films-list--extra">
         <h2 class="films-list__title">Top rated</h2>
   
         <div class="films-list__container">
-          ${getCardTemplate()}
-          ${getCardTemplate()}
+
         </div>
       </section>
   
@@ -31,8 +22,7 @@ export const getFilmsTemplate = () => {
         <h2 class="films-list__title">Most commented</h2>
   
         <div class="films-list__container">
-          ${getCardTemplate()}
-          ${getCardTemplate()}
+
         </div>
       </section>
     </section>
--- src/main.js
+++ src/main.js
@@ -3,19 +3,35 @@ import {getProfileTemplate} from "./components/profile";
 import {getMenuTemplate} from "./components/menu";
 import {getSortTemplate} from "./components/sort";
 import {getFilmsTemplate} from "./components/films";
-// import {getPopupTemplate} from "./components/popup";
+import {getCardTemplate} from "./components/film-card";
+import {getBtnMoreTemplate} from "./components/btn-more";
+import {getPopupTemplate} from "./components/popup";
 
-const headerElement = document.querySelector(`header`);
-const mainElement = document.querySelector(`main`);
-// const footerElement = document.querySelector(`footer`);
+const NUMBER_CARDS = [5, 2, 2];
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
 
+const headerElement = document.querySelector(`header`);
 render(headerElement, getSearchTemplate());
 render(headerElement, getProfileTemplate());
+
+const mainElement = document.querySelector(`main`);
 render(mainElement, getMenuTemplate());
 render(mainElement, getSortTemplate());
 render(mainElement, getFilmsTemplate());
-// render(footerElement, getPopupTemplate());
+
+const filmsListElement = mainElement.querySelector(`.films-list`);
+render(filmsListElement, getBtnMoreTemplate());
+
+const filmsListContainerElement = mainElement.querySelectorAll(`.films-list__container`);
+
+filmsListContainerElement.forEach((it, index) => {
+  for (let i = 0; i < NUMBER_CARDS[index]; i++) {
+    render(it, getCardTemplate());
+  }
+});
+
+const footerElement = document.querySelector(`footer`);
+render(footerElement, getPopupTemplate());
