d6a8d59f44a9723c850797e409c0edc153e3a71b
denis.e <denmetall@yandex.ru>
2019-08-15 14:14:44 +0700

    done more easy formatting for generate description film

--- src/data/cards.js
+++ src/data/cards.js
@@ -42,7 +42,13 @@ export const generateFilmData = () => {
       `the-great-flamarion.jpg`,
       `the-man-with-the-golden-arm.jpg`
     ][Math.floor(Math.random() * 7)],
-    description: new Array(1 + Math.floor(Math.random() * 2)).fill(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`.split(`. `)[Math.floor(Math.random() * 11)]).join(`. `),
+    description:
+      new Array(1 + Math.floor(Math.random() * 2))
+        .fill(
+            `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`
+            .split(`. `)[Math.floor(Math.random() * 11)]
+        )
+        .join(`. `),
     numberComments: Math.floor(Math.random() * 10),
     controls: {
       isAddedToWatchlist: Boolean(Math.round(Math.random())),
